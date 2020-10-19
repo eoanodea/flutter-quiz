f9d890778b8593646ced77c2eb6ec474cc5d20ec
Eoan O Dea <odeaeoan@gmail.com>
2020-10-19 22:20:00 +0200

    added scoreKeeper, questions & answers and added score & reset functions to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(Quizzler());
@@ -25,8 +27,93 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<bool> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+  List<bool> answers = [false, true, true];
+
+  /// Adds an icon as either a check or a close icon
+  /// to the [scoreKeeper] list
+  /// depending on what the user provides
+  void addScore(bool isCheck) {
+    bool result = false;
+    if (isCheck == answers[scoreKeeper.length]) result = true;
+
+    //   result = Icon(Icons.check, color: Colors.green);
+    // else
+    //   result = Icon(Icons.close, color: Colors.red);
+
+    setState(() => scoreKeeper.add(result));
+  }
+
+  void reset() {
+    setState(() => scoreKeeper.clear());
+  }
+
+  Widget renderAnswerPage() {
+    int result = 0;
+    int numOfQuestions = questions.length;
+    scoreKeeper.forEach((el) => {if (el) result++});
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text(
+          "Your Score:",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Card(
+          child: Container(
+            alignment: Alignment.center,
+            child: ListTile(
+              title: Text(
+                "$result / $numOfQuestions",
+                style: TextStyle(
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              subtitle: Text("Answers correct"),
+            ),
+          ),
+        ),
+        FlatButton(
+            color: Colors.blue,
+            child: Text(
+              'Start Again',
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+            ),
+            onPressed: () => reset()),
+      ],
+    );
+  }
+
+  renderScore() {
+    List<Widget> result = [];
+    scoreKeeper.forEach((score) {
+      if (score == true)
+        result.add(Icon(Icons.check, color: Colors.green));
+      else
+        result.add(Icon(Icons.close, color: Colors.red));
+    });
+
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (scoreKeeper.length == questions.length) return renderAnswerPage();
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -37,7 +124,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[scoreKeeper.length],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -51,51 +138,34 @@ class _QuizPageState extends State<QuizPage> {
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+                textColor: Colors.white,
+                color: Colors.green,
+                child: Text(
+                  'True',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
                 ),
-              ),
-              onPressed: () {
-                //The user picked true.
-              },
-            ),
+                onPressed: () => addScore(true)),
           ),
         ),
         Expanded(
           child: Padding(
             padding: EdgeInsets.all(15.0),
             child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
+                color: Colors.red,
+                child: Text(
+                  'False',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              onPressed: () {
-                //The user picked false.
-              },
-            ),
+                onPressed: () => addScore(false)),
           ),
         ),
-        Row(
-          children: <Widget>[
-            Icon(
-              Icons.check,
-              color: Colors.green,
-            ),
-            Icon(
-              Icons.close,
-              color: Colors.red,
-            ),
-          ],
-        )
+        Row(children: renderScore())
       ],
     );
   }
@@ -105,4 +175,9 @@ class _QuizPageState extends State<QuizPage> {
 question1: 'You can lead a cow down stairs but not up stairs.', false,
 question2: 'Approximately one quarter of human bones are in the feet.', true,
 question3: 'A slug\'s blood is green.', true,
+
+
+ 'You can lead a cow down stairs but not up stairs.', false,
+ 'Approximately one quarter of human bones are in the feet.', true,
+ 'A slug\'s blood is green.', true,
 */
