b6a74917a1f877fa4377e453408a7e9dfa7c5212
Eoan O Dea <odeaeoan@gmail.com>
2020-10-20 16:55:37 +0200

    Implemented question class to represent a single question

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz/question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -28,19 +27,20 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<bool> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(
+        q: 'Approximately one quarter of human bones are in the feet.',
+        a: true),
+    Question(q: 'A slug\'s blood is green.', a: true),
   ];
-  List<bool> answers = [false, true, true];
 
   /// Adds an icon as either a check or a close icon
   /// to the [scoreKeeper] list
   /// depending on what the user provides
   void addScore(bool isCheck) {
     bool result = false;
-    if (isCheck == answers[scoreKeeper.length]) result = true;
+    if (isCheck == questions[scoreKeeper.length].questionAnswer) result = true;
 
     setState(() => scoreKeeper.add(result));
   }
@@ -124,7 +124,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[scoreKeeper.length],
+                questions[scoreKeeper.length].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
--- lib/question.dart
+++ lib/question.dart
@@ -1,1 +1,18 @@
-class Question {}
+/// Represents a single question
+class Question {
+  String questionText;
+  bool questionAnswer;
+  bool answerCorrect;
+
+  Question({String q, bool a}) {
+    questionText = q;
+    questionAnswer = a;
+    answerCorrect = false;
+  }
+
+  setAnswer({bool a}) {
+    bool result = false;
+    if (a == questionAnswer) result = true;
+    answerCorrect = result;
+  }
+}
